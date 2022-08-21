3f083ae30203201119b649cd6e58a0d176732142
Prerna Malik <85059824+prernamalik701@users.noreply.github.com>
2022-08-21 18:21:03 +0530

    Add files via upload

---
+++ print a table.py
@@ -0,0 +1,10 @@
+t=input("what type of table would you like to display here?")
+t=int(t)
+print(t,"x",1,"=",t*1)
+t=input("what type of table would you like to display here?")
+t=int(t)
+for i in range(10):
+    print(t,"x",i,"=",t*i)
+
+    
+    
